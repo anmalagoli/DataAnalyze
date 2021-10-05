c3f41105c324500522c548159a6399eb53b26301
Andrea MALAGOLI <andrea.malagoli.elastyc@unimore.it>
2021-10-05 13:02:45 +0200

    impostato filter correttamente - testo in inglese

--- script.py
+++ script.py
@@ -9,9 +9,10 @@ import pandas as pd
 import numpy as np 
 from numpy import* 
 import matplotlib.pyplot as plt 
-from scipy.signal import butter, lfilter 
+from scipy.signal import butter, lfilter, filtfilt
 from scipy.fft import fft,fftfreq  
 import math
+import os
 
 #VARIABILI GLOBALI:
 sf=0
@@ -32,7 +33,7 @@ class window_Tk():
     def windowCostruction(self):
         self.window.attributes('-fullscreen',True)
         self.window.title('ANALYSIS')
-        self.window.resizable(False, False)
+        self.window.resizable(True, True)
         self.window.configure(bg='#f2bac7')
 
     def LabelCostruction(self,title,Xl,Yl,colour,arg,Xt,Yt):
@@ -88,30 +89,39 @@ class window_Tk():
         global filename
         filename = fd.askopenfilename(
             title='Open a file',
-            initialdir='/',
+            initialdir=os.getcwd(),
             filetypes=filetypes)
     
-        
     def close(self):
         window.destroy()
 
     def filteredGraph(self):
-        x = np.arange(0,len(self.y_temp)/800,1/800)
-        T = 1/int(sf.get())
-        n = int(T * int(sf.get())) 
+        freq = int(sf.get())
+        x = np.arange(0, len(self.y_temp)/freq, 1/freq)
+        T = 1/freq
+        n = int(T * freq)
         if len(self.__class__.y)==len(self.__class__.ordinata_x):
             if pf.get()=='LOW PASS':
-                self.__class__.y = self.butter_lowpass_filter(self.y_temp, float(lf.get()),int(sf.get()))
+                self.__class__.y = self.butter_lowpass_filter(
+                    self.y_temp, float(lf.get()), freq)
             elif pf.get()=='HIGH PASS':
-                self.__class__.y = self.butter_highpass_filter(self.y_temp,float(lf.get()),int(sf.get()))
+                self.__class__.y = self.butter_highpass_filter(
+                    self.y_temp, float(lf.get()), freq)
             elif pf.get()=='BAND PASS':
-                self.__class__.y=self.butter_bandpass_filter(self.__class__.y_temp,float(hf.get()),float(lf.get()),int(sf.get())) 
+                self.__class__.y = self.butter_bandpass_filter(
+                    self.__class__.y_temp, float(hf.get()), float(lf.get()), freq)
             elif pf.get()=='NO FILTERS':
                 self.__class__.y=self.__class__.y_temp
             
-            plt.plot(x, self.y_temp, 'b-', label='data')
-            plt.plot(x, self.__class__.y, 'g-', linewidth=2, label='filtered data')
-            plt.xlabel('Time [sec]')
+            fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+            ax1.plot(x, self.y_temp, 'b-', label='data')
+            ax1.set_title('Original signal at %dHz' %(int(sf.get())))
+            ax1.set_xlabel('Time [sec]')
+
+            ax2.plot(x, self.__class__.y, 'g-', linewidth=2, label='filtered data')
+            ax2.set_title('Filtered signal')
+            ax2.set_xlabel('Time [sec]')
+            
             plt.grid()
             plt.legend()
             plt.show()
@@ -119,83 +129,103 @@ class window_Tk():
     def spectrumGraph(self):
         T = 1.0 / int(sf.get())
 
+
+        fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+
         #graph of the spectrum without filters
         N = len(self.__class__.y_temp)
         media=mean(self.__class__.y_temp)
-        ordinata_y_filtrata_media=self.__class__.y_temp-media
-        n=log2(len(ordinata_y_filtrata_media))
+        y_media=self.__class__.y_temp-media
+        n=log2(len(y_media))
         n=math.ceil(n)
-        ordinata_y_filtrata_media = np.append(ordinata_y_filtrata_media, np.repeat(0,(pow(2,n))-N))
-        yf = fft( ordinata_y_filtrata_media)
+        y_media = np.append(y_media, np.repeat(0,(pow(2,n))-N))
+        yf = fft( y_media)
         N = pow(2, n)
         xf = fftfreq(N, T)[:N//2]
-        plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+        ax1.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
         
         #graph of the spectrum with filters
         N = len(self.__class__.y) 
         media=mean(self.__class__.y)
-        ordinata_y_filtrata_media=self.__class__.y-media
-        n=log2(len(ordinata_y_filtrata_media))
+        y_media=self.__class__.y-media
+        n=log2(len(y_media))
         n=math.ceil(n)
-        ordinata_y_filtrata_media = np.append(ordinata_y_filtrata_media, np.repeat(0,(pow(2,n))-N))
-        yf = fft( ordinata_y_filtrata_media)
+        y_media = np.append(y_media, np.repeat(0,(pow(2,n))-N))
+        yf = fft( y_media)
         N = pow(2, n)
         xf = fftfreq(N, T)[:N//2]
-        plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+        ax2.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
         
+        #plt.tight_layout()
         plt.show()
 
 
     def butter_lowpass(self,cutoff, fs, order=3):
         nyq = 0.5 * fs
         normal_cutoff = cutoff / nyq
-        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        #b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        b, a = butter(order, cutoff, btype='low', analog=False, fs=fs)
         return b, a
+
     def butter_lowpass_filter(self,data, cutoff, fs, order=3):
         b, a = self.butter_lowpass(cutoff, fs, order=order)
-        y = lfilter(b, a, data)
+       # y = lfilter(b, a, data)
+        y = filtfilt(b, a, data)
         return y
 
     def butter_highpass(self,cutoff, fs, order=3):
         nyq = 0.5 * fs
         normal_cutoff = cutoff / nyq
-        b, a = butter(order, normal_cutoff, btype='high', analog=False)
+        #b, a = butter(order, normal_cutoff, btype='high', analog=False)
+        b, a = butter(order, cutoff, btype='high', analog=False, fs=fs)
         return b, a
+
     def butter_highpass_filter(self,data, cutoff, fs, order=3):
         b, a = self.butter_highpass(cutoff, fs, order=order)
-        y = lfilter(b, a, data)
+        #y = lfilter(b, a, data)
+        y = filtfilt(b, a, data)
         return y
 
     def butter_bandpass(self, highcut, lowcut, fs, order=3):
         nyq = 0.5 * fs
         low = lowcut / nyq
         high = highcut / nyq
-        b, a = butter(order, [high,low ], btype='band', analog=False)
+        #b, a = butter(order, [high, low], btype='band', analog = True)
+        b, a = butter(order, [highcut, lowcut], btype='band', analog=False, fs=fs)
         return b, a
-    def butter_bandpass_filter(self,data, highcut, lowcut,  fs, order=3):
-        b, a = self.butter_bandpass(highcut, lowcut,  fs, order=order)
-        y = lfilter(b, a, data)
+
+    def butter_bandpass_filter(self, data, cutOff, cutOn, fs, order=4):
+        b, a = self.butter_bandpass(cutOff, cutOn, fs, order=order)
+        #y = lfilter(b, a, data)
+        y = filtfilt(b, a, data)
         return y
 
     def verify(self):
-        cont=0
-        lista_testi=[ ]
+        cont = 0  # contatore per andare avanti se si è inserito tutto correttamente
+        lista_testi = []
         lista_testi.append(dc.get())
         lista_testi.append(sf.get())
         lista_testi.append(sd.get())
         lista_testi.append(hf.get())
         lista_testi.append(lf.get())
         for i in lista_testi:
-            cont+=1
-            if len(i)==0:
-                messagebox.showinfo(f'ATTENZIONE','NON HAI INSERITO TUTTI I DATI ')
+            cont += 1
+            if len(i) == 0:
+                messagebox.showinfo(f'ATTENTION', 'INSERT ALL DATA')
                 break
-        if cont==len(lista_testi):
-            messagebox.showinfo(f'BRAVO','DATI CARICATI!')
-        y_temp = pd.read_csv(filename,  skiprows = 1, sep = ',',  header=None)
-        self.__class__.y_temp=y_temp[int(dc.get())]
-        self.__class__.ordinata_x=np.arange(0,(len(self.__class__.y))/int(sf.get()),1/int(sf.get()))
-    
+        if cont == len(lista_testi):
+            messagebox.showinfo(f'GOOD', 'DATA LOADED!')
+        #ORDINATE:
+        y_temp = pd.read_csv(filename,  skiprows=int(
+            sd.get()), sep=',',  header=None, )
+        y_temp = pd.DataFrame(y_temp)
+        y_temp = y_temp.dropna()
+        self.__class__.y_temp = y_temp[int(dc.get())]
+        self.filteredGraph()
+        #ASCISSE:
+        self.__class__.ordinata_x = np.arange(
+            0, (len(self.__class__.y))/int(sf.get()), 1/int(sf.get()))
+
 if __name__=='__main__':
     window=tk.Tk()
     dialog_window=window_Tk(window)
@@ -207,7 +237,7 @@ if __name__=='__main__':
     dialog_window.LabelCostruction('LOW FILTER',100,570,'#f2bac7','lf',100,600)
     dialog_window.LabelCostruction('SAMPLE FREQUENCY',100,670,'#f2bac7','sf',100,700)
     
-    dialog_window.BottonCostruction('UPLOAD FILE FROM BROWSE',SUNKEN,'white','#f71d86','select_file',100,100)
+    dialog_window.BottonCostruction('UPLOAD FILE FROM BROWSER',SUNKEN,'white','#f71d86','select_file',100,100)
     dialog_window.BottonCostruction('CLOSE',SUNKEN,'white','#f71d86','close',1250,700)
     dialog_window.BottonCostruction('LOADING DATA',SUNKEN,'white','#f71d86','verify',750,700)
     dialog_window.BottonCostruction('GRAPH WITH FILTERED SIGNAL',SUNKEN,'white','#f71d86','filteredGraph',1050,700)
--- script_corretto.py
+++ script_corretto.py
@@ -6,6 +6,7 @@ from tkinter.constants import SUNKEN #per bordo label
 from tkinter.messagebox import showinfo
 from numpy import ceil, log2 #per finestre di dialogo
 import pandas as pd #per grafico
+
 #per grafico:
 import numpy as np #per grafico
 from numpy import* #per grafico
@@ -13,6 +14,9 @@ import matplotlib.pyplot as plt #per grafico
 from scipy.signal import butter, lfilter #per filtraggio
 from scipy.fft import fft,fftfreq    #per fourier
 import math
+import os
+
+from scipy.signal.signaltools import filtfilt
 #VARIABILI GLOBALI:
 sf=0
 sd=0
@@ -33,7 +37,7 @@ class window_Tk():
     def costruzione_finestra(self):
         self.window.attributes('-fullscreen',True)
         self.window.title('ANALYSIS')
-        self.window.resizable(False, False)
+        self.window.resizable(True, True)
         self.window.configure(bg='#f2bac7')
 
     def creazione_label(self,title,Xl,Yl,colour,arg,Xt,Yt):
@@ -90,7 +94,7 @@ class window_Tk():
         global filename
         filename = fd.askopenfilename(
             title='Open a file',
-            initialdir='/',
+            initialdir=os.getcwd(),
             filetypes=filetypes)
     
         
@@ -102,7 +106,7 @@ class window_Tk():
             plt.plot(self.__class__.ordinata_x,self.__class__.ordinata_y_filtrata)
             plt.show()
         else:
-            messagebox.showinfo('ATTENTION','ERROR DURANTE LA CREAZIONE DEL GRAFICO, RICONTROLLARE I DATI')
+            messagebox.showinfo('ATTENTION','ERROR DURING PLOT: CHECK YOUR DATA')
 
     def disegna_grafico_spettro(self):
         N = len(self.__class__.ordinata_y_filtrata)  # Number of sample points
@@ -126,35 +130,43 @@ class window_Tk():
 
     def butter_lowpass(self,cutOff, fs, order=5):
         nyq = 0.5 * fs
-        normalCutoff = cutOff / nyq
-        b, a = butter(order, normalCutoff, btype='low', analog = True)
+        normalCutOff = cutOff / nyq
+        #b, a = butter(order, normalCutOff, btype='low', analog = True)
+        b, a = butter(order, cutOff, btype='low', analog=False, fs = fs)
         return b, a
 
     def butter_lowpass_filter(self,data, cutOff, fs, order=4):
         b, a = self.butter_lowpass(cutOff, fs, order=order)
-        y = lfilter(b, a, data)
+        #y = lfilter(b, a, data)
+        y = filtfilt(b, a, data)
         return y
 
     def butter_highpass(self,cutOn, fs, order=5):
         nyq = 0.5 * fs
         normalCutOn = cutOn / nyq
-        b, a = butter(order, normalCutOn, btype='low', analog = True)
+        #b, a = butter(order, normalCutOn, btype='high', analog = True)
+        b, a = butter(order, cutOn, btype='high', analog=False, fs = fs)
         return b, a
 
     def butter_highpass_filter(self,data, cutOn, fs, order=4):
         b, a = self.butter_highpass(cutOn, fs, order=order)
-        y = lfilter(b, a, data)
+        #y = lfilter(b, a, data)
+        y = filtfilt(b, a, data)
         return y
 
     def butter_bandpass(self,cutOff,cutOn, fs, order=5):
         nyq = 0.5 * fs
         normalbandoff = (cutOn-cutOff) / nyq
-        b, a = butter(order, normalbandoff, btype='low', analog = True)
+        normalCutOn = cutOn / nyq
+        normalCutOff = cutOff / nyq
+        #b, a = butter(order, [normalCutOn, normalCutOff], btype='band', analog = True)
+        b, a = butter(order, [cutOff, cutOn], btype='band', analog=False, fs = fs)
         return b, a
 
     def butter_bandpass_filter(self,data, cutOff,cutOn, fs, order=4):
         b, a = self.butter_bandpass(cutOff,cutOn, fs, order=order)
-        y = lfilter(b, a, data)
+        #y = lfilter(b, a, data)
+        y = filtfilt(b, a, data)
         return y
 
     def filtraggio(self):
@@ -178,12 +190,14 @@ class window_Tk():
         for i in lista_testi:
             cont+=1
             if len(i)==0:
-                messagebox.showinfo(f'ATTENZIONE','NON HAI INSERITO TUTTI I DATI ')
+                messagebox.showinfo(f'ATTENTION','INSERT ALL DATA')
                 break
         if cont==len(lista_testi):
-            messagebox.showinfo(f'BRAVO','DATI CARICATI!')
+            messagebox.showinfo(f'GOOD','DATA LOADED!')
         #ORDINATE:
-        y_temp = pd.read_csv(filename,  skiprows = 1, sep = ',',  header=None)
+        y_temp = pd.read_csv(filename,  skiprows = int(sd.get()), sep = ',',  header=None, )
+        y_temp = pd.DataFrame(y_temp)
+        y_temp = y_temp.dropna()
         self.__class__.ordinata_y_temp=y_temp[int(dc.get())]
         self.filtraggio()
         #ASCISSE:
@@ -203,11 +217,11 @@ if __name__=='__main__':
     finestra.creazione_label('LOW FILTER',100,570,'#f2bac7','lf',100,600)
     finestra.creazione_label('SAMPLE FREQUENCY',100,670,'#f2bac7','sf',100,700)
     
-    finestra.creazione_bottoni('CARICA FILE DAL BROWSE',SUNKEN,'white','#f71d86','select_file',100,100)
-    finestra.creazione_bottoni('CHIUDI',SUNKEN,'white','#f71d86','chiudi',1250,700)
-    finestra.creazione_bottoni('CARICAMENTO DATI',SUNKEN,'white','#f71d86','verifica',750,700)
-    finestra.creazione_bottoni('GRAFICO CON SEGNALE FILTRATO',SUNKEN,'white','#f71d86','disegna_grafico_filtrato',1050,700)
-    finestra.creazione_bottoni('GRAFICO DELLO SPETTRO',SUNKEN,'white','#f71d86','disegna_grafico_spettro',890,700)
+    finestra.creazione_bottoni('LOAD FILE FROM BROWSER',SUNKEN,'white','#f71d86','select_file',100,100)
+    finestra.creazione_bottoni('CLOSE',SUNKEN,'white','#f71d86','chiudi',1250,700)
+    finestra.creazione_bottoni('LOAD DATA',SUNKEN,'white','#f71d86','verifica',750,700)
+    finestra.creazione_bottoni('PLOT SIGNALS',SUNKEN,'white','#f71d86','disegna_grafico_filtrato',1050,700)
+    finestra.creazione_bottoni('PLOT SPECTRUM (Hz)',SUNKEN,'white','#f71d86','disegna_grafico_spettro',890,700)
 
     
     window.mainloop()
